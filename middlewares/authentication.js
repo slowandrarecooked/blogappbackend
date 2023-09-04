fa80078205aadf0d825b33290b4ab117dcb24961
slowandrarecooked <airbendervineeth@gmail.com>
2023-09-04 22:12:59 +0530

    v2

--- middlewares/authentication.js
+++ middlewares/authentication.js
@@ -1,7 +1,7 @@
 const jwt = require("jsonwebtoken");
 require("dotenv").config();
 const authentication = (req, res, next) => {
-  const token = req.headers.authorization.split(" ")[1];
+  const token = req.headers.authorization?.split(" ")[1];
   try {
     jwt.verify(token, process.env.SECRET, (err, decoded) => {
       if (err) res.send({ msg: "auth failed", err: err });
