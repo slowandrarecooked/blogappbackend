c5aaffbb9b56b7356f3bc8914d2c466cc8d1eb1e
slowandrarecooked <airbendervineeth@gmail.com>
2023-09-05 11:27:07 +0530

    v2

--- server.js
+++ server.js
@@ -7,8 +7,8 @@ const { userController } = require("./controllers/user.controller");
 const cors = require("cors");
 const app = express();
 
-app.use(cors());
 app.use(express.json());
+app.use(cors());
 app.get("/", (req, res) => {
   res.json({ msg: "base api" });
 });
